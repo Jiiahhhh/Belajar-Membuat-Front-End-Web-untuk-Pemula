d0a6820ad76e1a3bd43d04610aa7711f56e3331b
DESKTOP-I450EA2\ijlal <ijlalnurhadi01@gmail.com>
2024-03-26 14:16:10 +0800

    BOM and DOM

---
+++ BOM-DOM/BOM-console.js
@@ -0,0 +1,11 @@
+console.log("Ini adalah console log");
+console.info("Ini adalah console info");
+console.warn("Ini adalah console warn");
+console.error("Ini adalah console error");
+
+function alert(name) {
+  console.log("Hello " + name);
+}
+
+alert("Budi");
+window.alert("Halo Budi");
